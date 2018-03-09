26038d0cf5a93c9a682373f291a41280b1209860
An Tran <anqtran@gatech.edu>
2018-03-09 17:36:11 -0500

    adding proof of work and mining method

--- blockchain.js
+++ blockchain.js
@@ -1,16 +1,33 @@
 const SHA256 = require("crypto-js/sha256");
 
+class Transaction{
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
 class Block {
-    constructor(index, timestamp, data, previousHash = '') {
-        this.index = index;
+    constructor(timestamp, transactions, previousHash = '') {
         this.previousHash = previousHash;
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash() {
-      return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("BLOCK MINED: " + this.hash);
     }
 }
 
@@ -18,20 +35,51 @@ class Block {
 class Blockchain{
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
 
     createGenesisBlock() {
-        return new Block(0, "01/01/2017", "Genesis block", "0");
+        return new Block(Date.parse("2017-01-01"), [], "0");
     }
 
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
-        this.chain.push(newBlock);
+    minePendingTransactions(miningRewardAddress){
+        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
+        block.mineBlock(this.difficulty);
+
+        console.log('Block successfully mined!');
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress, this.miningReward)
+        ];
+    }
+
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address){
+        let balance = 0;
+
+        for(const block of this.chain){
+            for(const trans of block.transactions){
+                if(trans.fromAddress === address){
+                    balance -= trans.amount;
+                }
+
+                if(trans.toAddress === address){
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance;
     }
 
     isChainValid() {
@@ -52,17 +100,16 @@ class Blockchain{
     }
 }
 
-let andyCoin = new Blockchain();
-andyCoin.addBlock(new Block(1, "20/07/2017", { amount: 4 }));
-andyCoin.addBlock(new Block(2, "20/07/2017", { amount: 8 }));
+let savjeeCoin = new Blockchain();
+savjeeCoin.createTransaction(new Transaction('address1', 'address2', 100));
+savjeeCoin.createTransaction(new Transaction('address2', 'address1', 50));
 
+console.log('\n Starting the miner...');
+savjeeCoin.minePendingTransactions('antran-address');
 
-console.log('Blockchain valid? ' + andyCoin.isChainValid());
+console.log('\nBalance of antran is', savjeeCoin.getBalanceOfAddress('antran-address'));
 
-console.log('Changing a block...');
-andyCoin.chain[1].data = { amount: 100 };
-// andyCoin.chain[1].hash = andyCoin.chain[1].calculateHash();
+console.log('\n Starting the miner again...');
+savjeeCoin.minePendingTransactions('antran-address');
 
-console.log("Blockchain valid? " + andyCoin.isChainValid());
-
-// console.log(JSON.stringify(andyCoin, null, 4));
+console.log('\nBalance of antran is', savjeeCoin.getBalanceOfAddress('antran-address'));
